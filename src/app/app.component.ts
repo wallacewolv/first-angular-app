e8c773c50ea30c476367fb3ed002b4b217c19bfa
wallace <wallace_wolv@hotmail.com>
2024-08-03 17:12:30 -0300

    feat: (Seção 2-18) Styling & Using Our Next Custom Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { UserComponent } from "./user/user.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, UserComponent],
   standalone: true
 })
 export class AppComponent {
