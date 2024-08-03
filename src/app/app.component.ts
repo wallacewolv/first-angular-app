b2d3dc4b1b5c4077695cacb92f0715c3cd4889d4
wallace <wallace_wolv@hotmail.com>
2024-08-03 20:49:59 -0300

    feat: (Seção 2-43) Legacy Angular: Using ngFor & ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,22 @@
-import { CommonModule } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 import { HeaderComponent } from './header/header.component';
+import { TasksComponent } from './tasks/tasks.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './utils/dummy/dummy-users';
 import { User } from './utils/model/user';
-import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [CommonModule, HeaderComponent, UserComponent, TasksComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
   standalone: true
 })
 export class AppComponent {
   users: Array<User> = DUMMY_USERS.USERS;
-  selectedUserId = '';
+  selectedUserId!: string;
 
   get selectedUser() {
     return this.users.find((user => user.id === this.selectedUserId));
