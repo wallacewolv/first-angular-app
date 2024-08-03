93f984a711d2a9fe20f864fff14cbe986da222b8
wallace <wallace_wolv@hotmail.com>
2024-08-03 17:35:50 -0300

    feat: (Seção 2-22) Property Binding & Outputting Computed Values

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../utils/dummy/dummy-users';
+import { CommonModule } from '@angular/common';
 
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.USERS.length);
 @Component({
   selector: 'app-user',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
 export class UserComponent {
-
+  selectedUser = DUMMY_USERS.USERS[randomIndex];
 }
