d3c98cb44859f9d61fd3894bfc11ffa2a7823cf7
wallace <wallace_wolv@hotmail.com>
2024-08-03 17:00:46 -0300

    feat: (Seção 2-16) Styling the Header Component & Adding An Image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [HeaderComponent],
+  standalone: true
 })
 export class AppComponent {
   title = 'first-angular-app';
