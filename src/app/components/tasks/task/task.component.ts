84bf0f62ea0ac914dda1b585bb947a28d8c368e0
wallace <wallace_wolv@hotmail.com>
2024-08-04 00:56:23 -0300

    feat: (Seção 2-56) Content Projection with ng-content

--- src/app/components/tasks/task/task.component.ts
+++ src/app/components/tasks/task/task.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 import { Task } from './../../../utils/model/task.model';
+import { CardComponent } from "../../../shared/card/card.component";
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CardComponent],
   templateUrl: './task.component.html',
   styleUrls: ['./task.component.css']
 })
--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,12 +1,14 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 import {  User } from '../../utils/model/user.model';
+import { CardComponent } from "../../shared/card/card.component";
 
 @Component({
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
+  imports: [CardComponent]
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
