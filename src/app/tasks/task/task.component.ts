1fdc3927d0dfe8dc64115fa1c3bb39615c47c352
wallace <wallace_wolv@hotmail.com>
2024-08-03 22:02:37 -0300

    feat: (Seção 2-45) Outputting User-specific Tasks

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Task } from 'src/app/utils/model/task';
 
 @Component({
   selector: 'app-task',
@@ -9,5 +10,5 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./task.component.css']
 })
 export class TaskComponent {
-
+  @Input() task!: Task;
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,15 +1,23 @@
+import { NgFor } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { TaskComponent } from "./task/task.component";
+
+import { DUMMY_TASKS } from '../utils/dummy/dummy-tasks';
+import { User } from '../utils/model/user';
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css'],
   standalone: true,
-  imports: [TaskComponent]
+  imports: [TaskComponent, NgFor]
 })
-export class TasksComponent  {
-  @Input() name?: string;
+export class TasksComponent {
+  @Input({ required: true }) user!: User;
 
-  constructor() { }
+  tasks = DUMMY_TASKS.TASKS;
+
+  get selectedUserTasks() {
+    return this.tasks.filter(task => task.userId === this.user.id);
+  }
 }
