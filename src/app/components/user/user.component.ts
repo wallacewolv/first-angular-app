3998c01203af88e6d1ebb97086cec717237c4707
wallace <wallace_wolv@hotmail.com>
2024-08-03 22:38:44 -0300

    feat: (Seção 2-48) Dynamic CSS Styling with Class Bindings

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -10,6 +10,7 @@ import {  User } from '../../utils/model/user.model';
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
 
   @Output() select = new EventEmitter<string>();
 
