975b6a8a0c666b1a69980793583299b6c7229093
wallace <wallace_wolv@hotmail.com>
2024-08-03 21:16:02 -0300

    feat: (Seção 2-44) Adding More Components to the Demo App

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,17 +1,15 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { TaskComponent } from "./task/task.component";
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css'],
-  standalone: true
+  standalone: true,
+  imports: [TaskComponent]
 })
-export class TasksComponent implements OnInit {
+export class TasksComponent  {
   @Input() name?: string;
 
   constructor() { }
-
-  ngOnInit() {
-  }
-
 }
